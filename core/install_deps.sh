f933ef2d3fa069936e44a7e7fdc80413c76f507b
Jianmin <jianmin@nfs.iscas.ac.cn>
2014-08-18 15:27:51 +0800

    Fix the lack of libudev.so.0

--- core/install_deps.sh
+++ core/install_deps.sh
@@ -48,6 +48,25 @@ function installDependenciesWithApt {
 
     apt-get install g++
 
+    # fix the lack of libudev.so.0.
+
+    paths=(
+    "/lib/x86_64-linux-gnu/libudev.so.1" # Ubuntu, Xubuntu, Mint
+    "/usr/lib64/libudev.so.1" # SUSE, Fedora
+    "/usr/lib/libudev.so.1" # Arch, Fedora 32bit
+    "/lib/i386-linux-gnu/libudev.so.1" # Ubuntu 32bit
+    )
+    for i in "${paths[@]}"
+    do
+        if [ -f $(dirname $i)/libudev.so.0 ] ; then
+            break
+        fi
+        if [ -f $i ]
+        then
+            ln -sf "$i" $(dirname $i)/libudev.so.0
+            break
+        fi
+    done
 }
 
 checkInstaller
