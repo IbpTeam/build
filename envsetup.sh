f3bd08b515c9bb670422e79ce7704544bac5c814
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-10-11 15:19:49 +0800

    fix bug:404 not found netlink

--- envsetup.sh
+++ envsetup.sh
@@ -203,6 +203,9 @@ function m()
     bash $T/build/core/build_nodejs.sh || return
     bash $T/build/core/build_node_modules.sh || return
     bash $T/build/core/link_modules_for_app.sh || return
+
+    cd $(gettop)/app/demo-rio/nodewebkit
+    npm link
 }
 
 function mm()
