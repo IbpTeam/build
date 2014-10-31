15f099adfc897b1cdde65c29dffe602759416ea8
Hu Yuanzhe <yuanzhe@iscas.ac.cn>
2014-10-31 12:14:53 +0800

    modify init rio

--- core/init_rio.sh
+++ core/init_rio.sh
@@ -4,11 +4,16 @@ CURRENTPATH=$(cd `dirname $0`; pwd)
 . $(cd `dirname $CURRENTPATH`; pwd)/envsetup.sh nosetenv
 setenv
 echo create resources directory...
+rioPath=~/.demo-rio
 resourcePath=~/.resources
 #If directory exists, remove it.
+if [ -e $rioPath ]; then
+  rm -rf $rioPath
+fi
 if [ -e $resourcePath ]; then
   rm -rf $resourcePath
 fi
+cd ~&& mkdir $rioPath&& cd $rioPath&& touch uniqueID.js
 cd ~&& mkdir $resourcePath&& cd $resourcePath
 cateArr=(music document video picture desktop contactDes musicDes documentDes videoDes pictureDes desktopDes)
 for cateDir in ${cateArr[@]}
