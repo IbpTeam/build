f33cbdeb4354a8a0990d50be6e2c4e082164082e
wangfeng <wangfeng@nfs.iscas.ac.cn>
2014-11-17 15:27:32 +0800

    Fix a bug

--- core/init_database.sh
+++ core/init_database.sh
@@ -42,7 +42,7 @@ CREATE TABLE document (is_delete INTEGER, URI TEXT, postfix TEXT, filename TEXT,
 CREATE TABLE music (is_delete INTEGER, URI TEXT, postfix TEXT, filename TEXT, id INTEGER PRIMARY KEY, size TEXT, path TEXT, album TEXT, composerName TEXT, actorName TEXT, createTime TEXT,createDev TEXT, lastAccessTime TEXT,lastAccessDev TEXT,lastModifyTime TEXT,lastModifyDev TEXT,others TEXT);\
 CREATE TABLE picture (is_delete INTEGER, URI TEXT, postfix TEXT, filename TEXT, id INTEGER PRIMARY KEY, size TEXT, path TEXT, location TEXT, createTime TEXT,createDev TEXT, lastAccessTime TEXT,lastAccessDev TEXT,lastModifyTime TEXT,lastModifyDev TEXT,others TEXT);\
 CREATE TABLE tags (id INTEGER PRIMARY KEY, file_URI TEXT, tag TEXT);\
-CREATE TABLE video (is_delete INTEGER, URI TEXT, postfix TEXT, name TEXT, path TEXT, id INTEGER PRIMARY KEY, size TEXT, type TEXT, createTime TEXT,createDev TEXT, lastAccessTime TEXT,lastAccessDev TEXT,lastModifyTime TEXT,lastModifyDev TEXT,others TEXT);\
+CREATE TABLE video (is_delete INTEGER, URI TEXT, postfix TEXT, filename TEXT, path TEXT, id INTEGER PRIMARY KEY, size TEXT, type TEXT, createTime TEXT,createDev TEXT, lastAccessTime TEXT,lastAccessDev TEXT,lastModifyTime TEXT,lastModifyDev TEXT,others TEXT);\
 INSERT INTO category VALUES('./frontend-dev/images/contacts.jpg',101,'Contact','联系人');\
 INSERT INTO category VALUES('./frontend-dev/images/pictures.png',102,'Picture','图片');\
 INSERT INTO category VALUES('./frontend-dev/images/videos.png',103,'Video','视频');\
