3a1f58202b03bc6957fb1bb8b6acb8e15424c6aa
xuweihong <xuweihong1990@hotmail.com>
2015-06-16 14:28:22 +0800

    decreaseGitwithSharp

--- core/init_rio.sh
+++ core/init_rio.sh
@@ -9,19 +9,20 @@ resourcePath=$basePath/resource
 typeSourcePath=$CROOT/app/demo-rio/nodewebkit/backend/data
 typePath=$configPath/custard_type
 name=`whoami`
-userConifg="var uniqueID=\"rio"$RANDOM"rio\";\nexports.uniqueID=uniqueID;\nvar Account=\""$name"\";\nexports.Account=Account;"
+#userConifg="var uniqueID=\"rio"$RANDOM"rio\";\nexports.uniqueID=uniqueID;\nvar Account=\""$name"\";\nexports.Account=Account;"
 #If directory exists, remove it.
 if [ -e $basePath ]; then
   rm -rf $basePath
 fi
 mkdir -p $basePath
 cd $basePath&& mkdir $configPath&& cd $configPath
-echo -e $userConifg > uniqueID.js
+#echo -e $userConifg > uniqueID.js
 cd $basePath&& mkdir $resourcePath&& cd $resourcePath
 cateArr=(music document video picture desktop other)
 for cateDir in ${cateArr[@]}
 do
-   mkdir $cateDir&& cd $cateDir&& mkdir data&& git init&& cd ..
+   #mkdir $cateDir&& cd $cateDir&& mkdir data&& git init&& cd ..
+    mkdir $cateDir&& cd $cateDir&& mkdir data && cd ..
 done
 #custard_type
 mkdir $typePath
