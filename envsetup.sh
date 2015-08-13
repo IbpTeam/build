aa8e8f16cb1167adf1c6b3994128f4442c811826
LiangGuanyu <lianggy0719@126.com>
2015-08-13 18:19:53 +0800

    WEB-101 add svcDebug cmd

--- envsetup.sh
+++ envsetup.sh
@@ -15,6 +15,7 @@ Invoke ". set_env" from your shell to add the following functions to your enviro
 - bp         Build package.
 - genlog:    Echo your code's contribution about all repositries into ~/LOG by default.
 - svcInit:   Initialize a service package.
+- svcDebug:  List|stop|restart running services.
 - generator: Generate proxy, stub and index based on an interface file.
 - h:         Show more help.
 - stops:     Stop running services.
@@ -642,6 +643,10 @@ function svcInit() {
 
   npm init && mkdir implements interface "test"
   echo --------------------- Service Initialize Finished ---------------------
+}
+
+function svcDebug() {
+  $CROOT/service/svcmgr/tools/debug.js $*
 }
 
 function generator() {
